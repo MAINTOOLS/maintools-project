66d5afcb37486e1352910ff4d9b5d95f05e6b45e
Alejandro Rueda <alejandrorueda96@hotmail.com>
2021-10-24 22:50:07 -0500

    pantalla principal de inicio de sesion usando Oauth

--- gestion-ventas/src/Components/login/Login.jsx
+++ gestion-ventas/src/Components/login/Login.jsx
@@ -1,7 +1,19 @@
-const Login = () =>{
+import React from "react";
+import GoogleLogin from "react-google-login";
+function Login() {
+    function responseGoogle(response){
+        console.log(response);
+    }
     return(
         <div>
-            Pagina principal para iniciar sesion
+            <h1>Inicio de sesión</h1>
+            <GoogleLogin
+                clientId="889938699203-ufrbrbdjjlllfal5f5qksrfqpa54aavf.apps.googleusercontent.com"
+                buttonText="Iniciar sesión"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
         </div>
     )
 } 
